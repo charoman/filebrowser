0deef470188543a4bcb085feec08856588898209
Roman <charoman@gmail.com>
2017-01-19 14:30:39 +0200

    ok

--- test.js
+++ test.js
@@ -1,5 +1,33 @@
 var readlineSync = require('readline-sync');
-var answer = readlineSync.question('Your name: ');
-readlineSync.keyInSelect(['menu 1','menu 2','menu 3'],'question');
-console.log(answer);
-process.exit(0);
+var menu = ['quit', 'show current', 'change current', 'create item', 'delete item', 'open'];
+var fs = [[0, null, 'root'], [1, 0, 'sub1'],[1, 0, 'sub2'],[1, 0, 'file.txt','sdfsdf']]
+var current = 0;
+
+(function () {
+    printCurrentFolder();
+    while (true) {
+        printMenu();
+    }
+})();
+
+function printMenu() {
+    var choice = readlineSync.keyInSelect(menu, 'make your choice:', { cancel: false });
+    switch (choice) {
+        case 0: quitProgram(); break;
+        case 1: printCurrentFolder(); break;
+    }
+}
+
+function printCurrentFolder() {
+    var output = [];
+    for (var i in fs) {
+        var item = fs[i];
+        if (item[0] == current) output.unshift(item[2] + '/');
+        else if (item[1] == current) output.push('├──' + item[2] + (!item[3] ? '/' : ''));
+    }
+    for (var i in output) console.log(output[i]);
+}
+
+function quitProgram() {
+    process.exit(0);
+}
