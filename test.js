b7a7663fa35678d3c1775c62e553361e2e9dd177
Roman <charoman@gmail.com>
2017-01-19 15:29:32 +0200

    ok

--- test.js
+++ test.js
@@ -1,6 +1,11 @@
 var readlineSync = require('readline-sync');
 var menu = ['quit', 'show current', 'change current', 'create item', 'delete item', 'open'];
-var fs = [[0, null, 'root'], [1, 0, 'sub1'],[1, 0, 'sub2'],[1, 0, 'file.txt','sdfsdf']]
+var fs = [
+    [0, null, 'root'],
+    [2, 0, 'file.txt', 'sdfsdf'],
+    [3, 0, 'sub2'],
+    [1, 0, 'sub1'],
+]
 var current = 0;
 
 (function () {
@@ -25,7 +30,12 @@ function printCurrentFolder() {
         if (item[0] == current) output.unshift(item[2] + '/');
         else if (item[1] == current) output.push('├──' + item[2] + (!item[3] ? '/' : ''));
     }
+    output.sort(function (a, b) { return (isFolder(a) && isFolder(b) ? 0 : isFolder(a) && !isFolder(b) ? -1 : 1); });
     for (var i in output) console.log(output[i]);
+}
+
+function isFolder(folderName) {
+    return folderName.lastIndexOf('/') == folderName.length - 1;
 }
 
 function quitProgram() {
